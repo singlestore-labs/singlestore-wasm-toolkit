146cc6e185c6d3ff439c91cb46cf563b2035d79f
Kyle Brown <kyleb@liquidrocketry.com>
2022-06-06 11:16:49 -0400

    sentiment-rust initial

---
+++ sentiment-rust/src/lib.rs
@@ -0,0 +1,21 @@
+wit_bindgen_rust::export!("sentiment.wit");
+
+struct Sentiment();
+
+impl sentiment::Sentiment for Sentiment {
+
+    fn sentiment(input: String) -> PolarityScores {
+        lazy_static::lazy_static! {
+            static ref ANALYZER: vader_sentiment::SentimentIntensityAnalyzer<'static> =
+                vader_sentiment::SentimentIntensityAnalyzer::new();
+        }
+
+        let scores = ANALYZER.polarity_scores(input.as_str());
+        PolarityScores {
+            compound: scores["compound"],
+            positive: scores["pos"],
+            negative: scores["neg"],
+            neutral: scores["neu"],
+        }
+    }
+}
