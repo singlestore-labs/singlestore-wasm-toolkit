7ef2d98adb07d7dab8022ffecee896ac5c5000b5
Bailey Hayes <bhayes@singlestore.com>
2022-07-07 14:23:09 -0400

    fix: share wasi-sdk and wasmtime install
    The previous version had differences in versions.
    
    Instead of creating clang aliases,
    this adds wasi-sdk to the beginning of the PATH.
    This technique is taken from common-debian.sh in vscode-devcontainers
    so that it is compatible with vscode and regular containers.

---
+++ docker/install-wasi-sdk.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+WASI_SDK_VERSION="${WASI_SDK_VERSION:=16.0}"
+WASI_SDK_URL="${WASI_SDK_URL:=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_SDK_VERSION}/wasi-sdk-${WASI_SDK_VERSION}.0-linux.tar.gz}"
+WASI_SDK_HOME="${WASI_SDK_HOME:=/opt/wasi-sdk/}"
+
+mkdir -p ${WASI_SDK_HOME}
+curl -L ${WASI_SDK_URL} | tar -xz -C ${WASI_SDK_HOME} --strip-components 1
+
+updaterc() {
+    if [ "${UPDATE_RC}" = "true" ]; then
+        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/bash.bashrc
+        fi
+        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/zsh/zshrc
+        fi
+    fi
+}
+
+# Add WASI-SDK bin to beginning of PATH in bashrc/zshrc files (unless disabled)
+# so that WASI  clang and clang++ take precedence over the system ones.
+updaterc "$(cat << EOF
+if [[ "\${PATH}" != *"\${WASI_SDK_HOME}/bin"* ]]; then export PATH="\${WASI_SDK_HOME}/bin:\${PATH}"; fi
+EOF
+)"
